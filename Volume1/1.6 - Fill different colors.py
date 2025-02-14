fb0615f216cef3df2ef667984260b57eb647287d
sumitrediwal123 <sumit.rediwal@orchids.edu.in>
2025-02-14 16:22:31 +0530

    Update 1.6 - Fill different colors.py

--- Volume1/1.6 - Fill different colors.py
+++ Volume1/1.6 - Fill different colors.py
@@ -1,3 +1,4 @@
+#Task:- Fill colour in the second circle using begin_fill and the end_fill.
 import turtle
 t = turtle.Turtle()
 t.shape("turtle")
@@ -6,8 +7,11 @@ t.pencolor("red")
 t.color("green")
 # Begin to fill color
 t.begin_fill()
-t.circle(50)
+t.circle(80)
 # End-filling color
 t.end_fill()
-t.circle(80)
+t.color("red")
+
+t.circle(50)
+
 turtle.done()
