65e7417f27e3a25975d6898e759cf0ef766e0a0f
sumitrediwal123 <sumit.rediwal@orchids.edu.in>
2025-02-17 15:17:10 +0530

    Add files via upload

---
+++ Volume1/2.3 - Drawing with Multiple Turtle.py
@@ -0,0 +1,15 @@
+import turtle
+
+t1 = turtle.Turtle() #Creating a first turtle object.
+t1.shape("turtle")
+t1.circle(50)
+t2 = turtle.Turtle() #Creating a second turtle object.
+t2.shape("triangle")
+t2.color("red")
+t2.penup()
+t2.forward(200)
+t2.pendown()
+
+#Task: Draw a square using turtle t2
+
+turtle.done()
