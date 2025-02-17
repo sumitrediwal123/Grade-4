3c14d5e4fa1f654710916cb96a5baaef22247cd5
sumitrediwal123 <sumit.rediwal@orchids.edu.in>
2025-02-17 14:59:06 +0530

    Add files via upload

---
+++ Volume1/2.1 Move the turtle at specific location goto(x,y).py
@@ -0,0 +1,23 @@
+#Task 1: Create third turtle of square shape of blue color and move it to (100,100)
+#Task 2: Create fourth turtle of circle shape of orange color and move it to (-100,100)
+
+import turtle
+#First turtle
+t1=turtle.Turtle()
+t1.shape("turtle")
+t1.goto(100,0)
+#Second turtle
+t2=turtle.Turtle()
+t2.shape("triangle")
+t2.color("red")
+t2.goto(0,100)
+#Third turtle
+
+
+
+
+#Fourth turtle
+
+
+
+turtle.done()
