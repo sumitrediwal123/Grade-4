18a5552a640bdf55c6cc1e4e5dcbd842f8150841
sumitrediwal123 <sumit.rediwal@orchids.edu.in>
2025-02-17 15:14:48 +0530

    Add files via upload

---
+++ Volume1/2.2 - Multiple turtles.py
@@ -0,0 +1,12 @@
+import turtle
+
+t1 = turtle.Turtle() #Creating a first turtle object.
+t1.shape("turtle")
+t2 = turtle.Turtle() #Creating a second turtle object.
+t2.shape("triangle")
+t2.color("red")
+
+#Task: Move the second turtle to a different place
+
+
+turtle.done()
