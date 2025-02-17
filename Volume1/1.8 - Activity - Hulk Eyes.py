ed6dd469cf9de037d91241cc3175333a21e0543b
sumitrediwal123 <sumit.rediwal@orchids.edu.in>
2025-02-17 12:11:29 +0530

    Update 1.8 - Activity - Hulk Eyes.py

--- Volume1/1.8 - Activity - Hulk Eyes.py
+++ Volume1/1.8 - Activity - Hulk Eyes.py
@@ -1,3 +1,5 @@
+#Task : Create another Hulk eye.
+#Hint : Take help from line 8 to 11.
 import turtle
 t = turtle.Turtle()
 t.shape("turtle")
@@ -7,6 +9,7 @@ t.begin_fill()
 t.circle(30)
 t.end_fill()
 t.circle(50)
+
 t.forward(150)
 
 #Write code of another eye on the next line.
