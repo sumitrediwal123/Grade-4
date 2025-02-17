98cfdec4e13e8ef774c7232d3f79fdf60f8dac9d
sumitrediwal123 <sumit.rediwal@orchids.edu.in>
2025-02-17 12:00:33 +0530

    Add files via upload

---
+++ Volume1/2.1 - Forward,Backward,Left,Right.py
@@ -0,0 +1,18 @@
+import turtle
+t=turtle.Turtle()
+t.shape("turtle")
+t.left(90)
+
+#Task 1: Turn the turtle left again
+
+#Task 2: Move the turtle forward
+
+#Task 3: Turn the turtle right
+
+#Task 4: Move the turtle forward again
+
+
+turtle.done()
+
+
+
