49d5eeb9bc724748097542a75eddbd83a16b6b31
sumitrediwal123 <sumit.rediwal@orchids.edu.in>
2025-02-28 16:11:41 +0530

    Update 1.7 - Movement of turtle(forward).py

--- Volume1/1.7 - Movement of turtle(forward).py
+++ Volume1/1.7 - Movement of turtle(forward).py
@@ -1,9 +1,9 @@
-#Task:-  Move the turtle for 300 pixels and create the circle of radius 100 after moving.
+#Task:-  Move the turtle forward by 300 steps and create the circle of radius 100 after moving.
 import turtle
 t = turtle.Turtle()
 t.pencolor("red")
 t.shape("turtle")
-# turtle will move forward 100 pixels
+# turtle will move forward 100 steps
 t.forward(100)
 #Create a circle
 
