d66d1e60f557f3359526cda12b8f37e890373192
sumitrediwal123 <sumit.rediwal@orchids.edu.in>
2025-03-01 17:45:17 +0530

    Update and rename 1.4 - Give pen color.py to 2.4 - Give pen color.py

--- Volume1/2.4 - Give pen color.py
+++ Volume1/2.4 - Give pen color.py
@@ -1,8 +1,8 @@
-#Task:- Change the pen color to “green”
+#Task:- Change the pen colour to “green”
 import turtle
 t = turtle.Turtle()
 t.shape("turtle")
-# Changing the pen color to red
+# Changing the pen colour to red
 t.pencolor("red")
 t.circle(50)
 turtle.done()
