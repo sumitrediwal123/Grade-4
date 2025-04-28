e18b6b27d56a34d4808965cde3385152fb8fe0bc
sumitrediwal123 <sumit.rediwal@orchids.edu.in>
2025-04-28 15:18:41 +0530

    Update 2.8 - Forward,Backward,Left,Right.py

--- Volume1/2.8 - Forward,Backward,Left,Right.py
+++ Volume1/2.8 - Forward,Backward,Left,Right.py
@@ -1,15 +1,21 @@
 import turtle
 t=turtle.Turtle()
 t.shape("turtle")
-t.left(90)
 
-#Task 1: Turn the turtle left again
+#Task 1: Move the turtle forward 100 steps
 
-#Task 2: Move the turtle forward
+#Task 2: Turn the turtle right 90 degrees
 
-#Task 3: Turn the turtle right
+#Task 3: Move the turtle forward 100 steps
 
-#Task 4: Move the turtle forward again
+#Task 4: Turn the turtle right 90 degrees
+
+#Task 5: Move the turtle forward 100 steps
+
+#Task 6: Turn the turtle left 90 degrees
+
+#Task 7: Move the turtle backward 100 steps
+
 
 
 turtle.done()
