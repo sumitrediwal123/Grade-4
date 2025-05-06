7e3efbf2589e3ee3d36956b21f7277e8710e8edc
sumitrediwal123 <sumit.rediwal@orchids.edu.in>
2025-05-06 11:38:13 +0530

    Update 3.2 - Multiple turtles.py

--- Volume1/3.2 - Multiple turtles.py
+++ Volume1/3.2 - Multiple turtles.py
@@ -1,3 +1,4 @@
+#Task: Write a code in line 10 to move the second turtle to a different place 
 import turtle
 
 t1 = turtle.Turtle() #Creating a first turtle object.
@@ -6,7 +7,6 @@ t2 = turtle.Turtle() #Creating a second turtle object.
 t2.shape("triangle")
 t2.color("red")
 
-#Task: Move the second turtle to a different place
 
 
 turtle.done()
